9d9ef143b52a164909b2de2bd1706375f1ad5132
Benjamin Httenberger <benjamin.huettenberger@gmail.com>
2016-06-01 23:01:54 +0200

    added error graph to visualize errors per hour

--- src/main/java/at/lanpoint/connectionChecker/Main/ViewController.java
+++ src/main/java/at/lanpoint/connectionChecker/Main/ViewController.java
@@ -7,7 +7,11 @@ import java.awt.TrayIcon;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javafx.stage.WindowEvent;
+import javafx.util.StringConverter;
+
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.ResourceBundle;
 import javax.imageio.ImageIO;
 
@@ -16,6 +20,10 @@ import javafx.application.Platform;
 import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.chart.AreaChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
@@ -23,20 +31,23 @@ import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 import javafx.event.EventHandler;
 
-public class ViewController implements Initializable{
-	
+public class ViewController implements Initializable {
+
 	@FXML
 	private ImageView statusImage;
-	
-	@FXML 
+
+	@FXML
 	private Label count;
-	
+
 	@FXML
 	private Button startButton;
-	
+
 	@FXML
 	private Button stopButton;
-	
+
+	@FXML
+	private Button graphButton;
+
 	private Image notOk = new Image(getClass().getResourceAsStream("/notok.png"));
 	private Image ok = new Image(getClass().getResourceAsStream("/ok.png"));
 
@@ -48,119 +59,168 @@ public class ViewController implements Initializable{
 	private TrayIcon icon = null;
 	private Boolean alreadyNotificatedOffline = false;
 	private Boolean alreadyNotificatedOnline = false;
-	
+	private Map<Integer, Integer> errorsDuringDay = new HashMap<Integer, Integer>();
+
 	public void initialize(URL location, ResourceBundle resources) {
 		statusImage.setImage(notOk);
 	}
-	
+
 	@FXML
-	private void startChecker(){
+	private void startChecker() {
 		bl.execute = true;
-		
+
 		thread = new Thread(bl);
 		thread.start();
-		
+
 		updateThread = new Thread(updateTask);
 		updateThread.start();
-		
-		Platform.runLater(new Runnable(){
+
+		Platform.runLater(new Runnable() {
 
 			public void run() {
 				statusImage.setImage(ok);
 				startButton.setDisable(true);
 			}
-			
+
 		});
 		execute = true;
 	}
-	
+
 	@FXML
-	private void stopChecker(){
+	private void stopChecker() {
 		bl.execute = false;
-		Platform.runLater(new Runnable(){
+		Platform.runLater(new Runnable() {
 
 			public void run() {
 				statusImage.setImage(notOk);
 				startButton.setDisable(false);
 			}
-			
+
 		});
 		execute = false;
 	}
-	
-	Task<Void> updateTask = new Task<Void>(){
+
+	@FXML
+	private void showChart() {
+		errorsDuringDay = bl.getErrorsDuringDay();
+		Stage stage = new Stage();
+		stage.setTitle("Anzahl der Fehler pro Stunde");
+		stage.getIcons().add(new Image(getClass().getResourceAsStream("/icon.png")));
+
+		final NumberAxis xAxis = new NumberAxis(1, 24, 1);
+		final NumberAxis yAxis = new NumberAxis();
+		
+		yAxis.setTickLabelFormatter(new IntegerStringConverter());
+
+		final AreaChart<Number, Number> ac = new AreaChart<Number, Number>(xAxis, yAxis);
+		ac.setTitle("Fehler pro Stunde");
+
+		XYChart.Series seriesErrors = new XYChart.Series();
+		seriesErrors.setName("Anzahl der Fehler");
+
+		for (int i = 1; i <= 24; i++) {
+			if (errorsDuringDay.size() >= 24) {
+				seriesErrors.getData().add(new XYChart.Data(i, errorsDuringDay.get(i)));
+			} else {
+				seriesErrors.getData().add(new XYChart.Data(i, 0));
+			}
+		}
+
+		Scene scene = new Scene(ac, 800, 600);
+		ac.getData().addAll(seriesErrors);
+		stage.setScene(scene);
+		stage.show();
+	}
+
+	/**
+	 * task which updates all gui variables
+	 */
+	Task<Void> updateTask = new Task<Void>() {
 
 		@Override
 		protected Void call() throws Exception {
-			
-			while(true){
-				if(execute){
-					try{
-						Platform.runLater(new Runnable(){
+
+			while (true) {
+				if (execute) {
+					try {
+						Platform.runLater(new Runnable() {
 							public void run() {
 								count.textProperty().setValue(bl.count.toString());
-								if(bl.error){
+								if (bl.error) {
 									statusImage.setImage(notOk);
-									//notification
-									if(!alreadyNotificatedOffline){
+									// notification
+									if (!alreadyNotificatedOffline) {
 										alreadyNotificatedOffline = true;
-										icon.displayMessage("ConnectionChecker", "Achtung der Computer ist offline!", TrayIcon.MessageType.WARNING);
+										icon.displayMessage("ConnectionChecker", "Achtung der Computer ist offline!",
+												TrayIcon.MessageType.WARNING);
 										alreadyNotificatedOnline = false;
 									}
-								}
-								else{
+								} else {
 									statusImage.setImage(ok);
-									//notification
-									if(!alreadyNotificatedOnline){
+									// notification
+									if (!alreadyNotificatedOnline) {
 										alreadyNotificatedOnline = true;
-										icon.displayMessage("ConnectionChecker", "Der Computer ist online!", TrayIcon.MessageType.INFO);
+										icon.displayMessage("ConnectionChecker", "Der Computer ist online!",
+												TrayIcon.MessageType.INFO);
 										alreadyNotificatedOffline = false;
 									}
 								}
+
+								errorsDuringDay = bl.getErrorsDuringDay();
 							}
 						});
 						Thread.sleep(1000);
-					}catch(Exception e){
+					} catch (Exception e) {
 						e.printStackTrace();
 					}
-				}else{
-					try{
+				} else {
+					try {
 						Thread.sleep(1000);
-					}catch(Exception e){
+					} catch (Exception e) {
 						e.printStackTrace();
 					}
 				}
 			}
 		}
 	};
-	
-	public void setStage(Stage stage){
+
+	/**
+	 * register application response on close request
+	 * 
+	 * @param stage
+	 */
+	public void setStage(Stage stage) {
 		this.controllerStage = stage;
-		
-		if(SystemTray.isSupported()){
+
+		if (SystemTray.isSupported()) {
 			Platform.setImplicitExit(false);
 			setTrayIcon(controllerStage);
-		}else{
-		
-			stage.setOnCloseRequest(new EventHandler<WindowEvent>(){
+		} else {
+
+			stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
 				public void handle(WindowEvent event) {
 					System.exit(0);
 				}
 			});
 		}
 	}
-	
-	private void setTrayIcon(final Stage primaryStage){
+
+	/**
+	 * sets tray icon of application
+	 * 
+	 * @param primaryStage
+	 */
+	private void setTrayIcon(final Stage primaryStage) {
 		SystemTray systemTray = SystemTray.getSystemTray();
 		URL url = getClass().getResource("/icon_small.png");
 		java.awt.Image simage = null;
-		try{
+		try {
 			simage = ImageIO.read(url);
-		}catch(Exception e){
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
-		//set action listers for popup menue
+
+		// set action listers for popup menue
 		ActionListener listenerShow = new ActionListener() {
 
 			public void actionPerformed(ActionEvent e) {
@@ -201,10 +261,27 @@ public class ViewController implements Initializable{
 
 		try {
 			systemTray.add(icon);
-		}
-		catch (Exception e) {
+		} catch (Exception e) {
 			System.err.println(e);
 		}
 
+	}
+
+	class IntegerStringConverter extends StringConverter<Number> {
+		public IntegerStringConverter() {
+		}
+
+		@Override
+		public String toString(Number object) {
+			if (object.intValue() != object.doubleValue())
+				return "";
+			return "" + (object.intValue());
+		}
+
+		@Override
+		public Number fromString(String string) {
+			Number val = Double.parseDouble(string);
+			return val.intValue();
+		}
 	}
 }
--- src/main/java/at/lanpoint/connectionChecker/businesslayer/BusinessLayer.java
+++ src/main/java/at/lanpoint/connectionChecker/businesslayer/BusinessLayer.java
@@ -1,5 +1,10 @@
 package at.lanpoint.connectionChecker.businesslayer;
 
+import java.time.LocalDateTime;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
+
 import at.lanpoint.connectionChecker.connection.ConnectionCheck;
 import at.lanpoint.connectionChecker.connection.IConnectionCheck;
 import at.lanpoint.connectionChecker.logger.ConnectionLogger;
@@ -7,22 +12,47 @@ import at.lanpoint.connectionChecker.logger.ConnectionLogger;
 public class BusinessLayer implements Runnable {
 	
 	private IConnectionCheck connectionCheck = new ConnectionCheck();
+	private Map<Integer, Integer> errorsDuringDay = new HashMap<Integer, Integer>(); //map records number of errors for each hour
 	public boolean execute = true;
 	public boolean error = false;
 	public Integer count = 0;
-
+	
+	public BusinessLayer(){
+		initialize();
+	}
+	
+	/**
+	 * initializes errors of Day
+	 */
+	private void initialize(){
+		for(int i = 1; i < 25; i++){
+			errorsDuringDay.put(i, 0);
+		}
+	}
+	
 	public void run() {
 		count = 0;
 		
 		while(execute){
-			if(connectionCheck.checkIfURLExists("http://www.google.com/")){
+			if(connectionCheck.checkIfURLExists("http://www.google.com/")){ 
 				ConnectionLogger.info("Ping ok");
 				error = false;
 			}
-			else{
+			else{ //no internet connection
 				ConnectionLogger.warning("Internet connection problem!");
 				error = true;
 				count++;
+				
+				//raise error on hour
+				int hour = LocalDateTime.now().getHour();
+				if(errorsDuringDay.containsKey(hour)){
+					int errorsOfHour = errorsDuringDay.get(hour);
+					errorsOfHour++;
+					errorsDuringDay.put(hour, errorsOfHour);
+				}
+				else{
+					errorsDuringDay.put(hour, 1);
+				}
 			}
 			
 			try {
@@ -31,5 +61,9 @@ public class BusinessLayer implements Runnable {
 				e.printStackTrace();
 			}	
 		}	
+	}
+	
+	public Map<Integer, Integer> getErrorsDuringDay() {
+		return errorsDuringDay;
 	}
 }
